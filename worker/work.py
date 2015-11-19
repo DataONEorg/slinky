e9f1a55dbbad2439a97fd649c12171236c322496
Bryce Mecum <petridish@gmail.com>
2015-11-18 16:32:51 -0900

    Clean up imports in worker script

--- worker/work.py
+++ worker/work.py
@@ -1,14 +1,9 @@
-import os
-import sys
 import time
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 from redis import StrictRedis
 from rq import Worker, Queue, Connection
-
-sys.path.append('/usr/local/d1lod')
-from d1lod import jobs
 
 conn = StrictRedis(host='redis', port='6379')
 
