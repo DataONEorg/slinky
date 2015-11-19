076fce6fdfe936d93106ca12c77e87f52cf7fa83
Bryce Mecum <petridish@gmail.com>
2015-11-18 17:06:59 -0900

    Start fleshing out the add dataset integration test

--- d1lod/tests/test_sesame_interface.py
+++ d1lod/tests/test_sesame_interface.py
@@ -10,6 +10,42 @@ def test_can_add_a_dataset():
     """Test whether the right triples are added when we add a known dataset.
 
     We pass the store to this test because we'll need to specify namespaces.
+
+    The dataset with PID: doi:10.6073/AA/knb-lter-cdr.70061.123
+
+    has related information
+    Solr: https://cn.dataone.org/cn/v1/query/solr/?q=id:*knb-lter-cdr.70061.123&rows=1&start=0
+    Meta: https://cn.dataone.org/cn/v1/meta/doi:10.6073/AA/knb-lter-cdr.70061.123
+    Object: https://cn.dataone.org/cn/v1/object/doi:10.6073/AA/knb-lter-cdr.70061.123
+
+    Dataset:
+
+    abstract: '...'
+    title: '...''
+    beginDate: '1989-01-01T00:00:00Z'
+    endDate: '2003-01-01T00:00:00Z'
+    originMN: urn:node:LTER
+    replicaMNs:
+        urn:node:CN
+        urn:node:LTER
+    authoritativeMN: urn:node:LTER
+
+    dateUploaded: 2012-03-03T00:00:00Z
+
+    eastBoundCoord: -93.16289
+    southBoundCoord: 45.384865
+    westBoundCoord: -93.22445
+    northBoundCoord: 45.44138
+
+    identifier;
+        doi:10.6073/AA/knb-lter-cdr.70061.123
+    obsoletes: doi:10.6073/AA/knb-lter-cdr.70061.122
+
+    Digital Object 1: Metadata
+    checksum: 4f3d47e4110c4bb7dd24d74bb81f005b
+    checksumAlgo: MD5
+    format: eml://ecoinformatics.org/eml-2.1.0
+    formattype: METADATA
     """
 
     namespaces = {
