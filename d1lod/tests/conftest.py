00c28d76d93331d7a501f0006cbadcaef48e499f
Bryce Mecum <petridish@gmail.com>
2015-12-07 12:02:16 -0900

    Add default set of namespaces to test repository instance

--- d1lod/tests/conftest.py
+++ d1lod/tests/conftest.py
@@ -8,7 +8,26 @@ def store():
 
 @pytest.fixture(scope="module")
 def repo(store):
-    return sesame.Repository(store, 'test')
+    namespaces = {
+        'owl': 'http://www.w3.org/2002/07/owl#',
+        'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'xsd': 'http://www.w3.org/2001/XMLSchema#',
+        'foaf': 'http://xmlns.com/foaf/0.1/',
+        'dcterms': 'http://purl.org/dc/terms/',
+        'datacite': 'http://purl.org/spar/datacite/',
+        'glbase': 'http://schema.geolink.org/',
+        'd1dataset': 'http://lod.dataone.org/dataset/',
+        'd1person': 'http://lod.dataone.org/person/',
+        'd1org': 'http://lod.dataone.org/organization/',
+        'd1node': 'https://cn.dataone.org/cn/v1/node/',
+        'd1landing': 'https://search.dataone.org/#view/',
+        "prov": "http://www.w3.org/ns/prov#"
+    }
+
+    repository = sesame.Repository(store, 'test', ns=namespaces)
+
+    return repository
 
 @pytest.fixture(scope="module")
 def interface(repo):
