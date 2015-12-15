55fbfeb8a84b85ff0e2fd9c2259ca2d1aa6ecc64
Bryce Mecum <mecum@mecum.local>
2015-12-15 13:54:46 -0900

    Reorder prefixes

--- d1lod/d1lod/jobs.py
+++ d1lod/d1lod/jobs.py
@@ -26,13 +26,12 @@ namespaces = {
     'foaf': 'http://xmlns.com/foaf/0.1/',
     'dcterms': 'http://purl.org/dc/terms/',
     'datacite': 'http://purl.org/spar/datacite/',
+    "prov": "http://www.w3.org/ns/prov#"
     'glbase': 'http://schema.geolink.org/',
     'd1dataset': 'http://lod.dataone.org/dataset/',
     'd1person': 'http://lod.dataone.org/person/',
     'd1org': 'http://lod.dataone.org/organization/',
-    'd1node': 'https://cn.dataone.org/cn/v1/node/',
-    "prov": "http://www.w3.org/ns/prov#"
-
+    'd1node': 'https://cn.dataone.org/cn/v1/node/'
 }
 
 SESAME_HOST = os.getenv('WEB_1_PORT_8080_TCP_ADDR', 'localhost')
