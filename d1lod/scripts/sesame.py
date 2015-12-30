7f026603b3268cd9d009d38e9fb6fc08fed6600f
Bryce Mecum <petridish@gmail.com>
2015-12-30 14:39:41 -0900

    Update Sesame script

--- d1lod/scripts/sesame.py
+++ d1lod/scripts/sesame.py
@@ -2,12 +2,16 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-from d1lod import dataone, validator, util
+from d1lod import dataone, validator, util, jobs
 from d1lod.people import processing
 from d1lod.sesame import Store, Repository, Interface
 
 if __name__ == '__main__':
+
+    logging.info("Main routine started.")
 
     namespaces = {
         'owl': 'http://www.w3.org/2002/07/owl#',
@@ -24,51 +28,43 @@ if __name__ == '__main__':
         'd1node': 'https://cn.dataone.org/cn/v1/node/',
         'd1landing': 'https://search.dataone.org/#view/',
         "prov": "http://www.w3.org/ns/prov#"
-
     }
 
     s = Store("localhost", 8080)
-    r = Repository(s, "test", namespaces)
+    r = Repository(s, "d1lod", namespaces)
     i = Interface(r)
 
-    r.query("SELECT * WHERE { ?s ?p ?o }")
-    # r.clear()
-    #
-    # identifier = 'doi:10.6073/AA/knb-lter-cdr.70061.123'
-    # doc = dataone.getSolrIndexFields(identifier)
-    # i.addDataset(doc)
+    i.addDataset('knb-lter-bes.462.56')
+
+    sys.exit()
+
+    from_string =   "2015-12-29T00:00:00.0Z"
+    to_string   =   jobs.getNowString()
+    print from_string
+    print to_string
 
 
+    query_string = dataone.createSinceQueryURL(from_string, to_string, None, 0)
+    num_results = dataone.getNumResults(query_string)
+    print num_results
 
+    page_size=1000
+    num_pages = num_results / page_size
 
+    if num_results % page_size > 0:
+        num_pages += 1
 
-    # identifier = 'doi:10.6085/AA/YB15XX_015MU12004R00_20080619.50.1'
-    # doc = dataone.getSolrIndexFields(identifier)
-    # i.addDataset(doc)
+    # Process each page
+    for page in range(1, num_pages + 1):
+        page_xml = dataone.getSincePage(from_string, to_string, page, page_size)
+        docs = page_xml.findall(".//doc")
 
-    #
-    # # with open("test.ttl", "wb") as f:
-    # #     f.write(r.export())
-    #
-    # from_string =   "2015-01-01T15:00:00.0Z"
-    # to_string   =   "2015-01-06T16:05:00.0Z"
-    #
-    # query_string = dataone.createSinceQueryURL(from_string, to_string, None, 0)
-    # print query_string
-    # num_results = dataone.getNumResults(query_string)
-    # print num_results
-    #
-    # page_size=1000
-    # num_pages = num_results / page_size
-    # if num_results % page_size > 0:
-    #     num_pages += 1
-    #
-    #
-    # for page in range(1, num_pages + 1):
-    #     print "Processing page %d." % page
-    #
-    #     page_xml = dataone.getSincePage(from_string, to_string, page=page, page_size=page_size)
-    #     docs = page_xml.findall(".//doc")
-    #
-    #     for doc in docs[10:19]:
-    #         i.addDataset(doc)
+        for doc in docs:
+            identifier = dataone.extractDocumentIdentifier(doc)
+            i.addDataset(identifier, doc)
+
+    # identifier = 'doi:10.6073/AA/knb-lter-arc.376.1'
+    # jobs.add_dataset(identifier)
+
+    # jobs.updateVoIDFile('test')
+    # jobs.setLastRun()
