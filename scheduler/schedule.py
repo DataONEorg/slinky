1ad279c0ce2ed44cb8e69c9492593a8635f56318
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-05-08 13:28:18 -0800

    Fix queue names in scheduler

--- scheduler/schedule.py
+++ scheduler/schedule.py
@@ -10,39 +10,34 @@ from d1lod import jobs
 
 conn = StrictRedis(host='redis', port='6379')
 queues = {
-    'low': Queue('low', connection=conn),
-    'medium': Queue('medium', connection=conn),
-    'high': Queue('high', connection=conn)
+    'default': Queue('default', connection=conn),
+    'dataset': Queue('dataset', connection=conn),
+    'export': Queue('export', connection=conn)
 }
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', id='update', minutes=1)
 def queue_update_job():
-    queues['medium'].enqueue(jobs.update_graph)
+    queues['default'].enqueue(jobs.update_graph)
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', id='stats', minutes=1)
 def queue_stats_job():
-    queues['medium'].enqueue(jobs.calculate_stats)
+    queues['default'].enqueue(jobs.calculate_stats)
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', id='export', hours=1)
 def queue_export_job():
-    queues['medium'].enqueue(jobs.export_graph)
-
-
-@sched.scheduled_job('interval', minutes=1)
-def print_jobs_job():
-    sched.print_jobs()
+    queues['export'].enqueue(jobs.export_graph)
 
 # Wait a bit for Sesame to start
 time.sleep(10)
 
 # Queue the stats job first. This creates the repository before any other
 # jobs are run.
-queues['high'].enqueue(jobs.calculate_stats)
+queues['default'].enqueue(jobs.calculate_stats)
 
 # Start the scheduler
 sched.start()
