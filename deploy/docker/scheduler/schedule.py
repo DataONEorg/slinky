683d625ccfb1537189ec486666aa66d770491175
Thomas Thelen <tommythelen@gmail.com>
2021-03-22 14:10:33 -0700

    Add the Dockerfiles for building the K8 requirements and add a Readme on using them.

---
+++ deploy/docker/scheduler/schedule.py
@@ -0,0 +1,50 @@
+import time
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from redis import StrictRedis
+from rq import Queue
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+import sys
+# The RDF package installs to the root of dist-packages...
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+# Manually add d1lod since it was copied & not installed with pip
+sys.path.append('/usr/lib/python2.7/dist-packages/d1lod')
+from d1lod import jobs
+
+conn = StrictRedis(host='redis', port='6379')
+queues = {
+    'default': Queue('default', connection=conn),
+    'dataset': Queue('dataset', connection=conn),
+    'export': Queue('export', connection=conn)
+}
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', id='update', minutes=1)
+def queue_update_job():
+    queues['default'].enqueue(jobs.update_graph)
+
+
+@sched.scheduled_job('interval', id='stats', minutes=1)
+def queue_stats_job():
+    queues['default'].enqueue(jobs.calculate_stats)
+
+
+@sched.scheduled_job('interval', id='export', hours=1)
+def queue_export_job():
+    queues['export'].enqueue(jobs.export_graph)
+
+# Wait a bit for Sesame to start
+time.sleep(10)
+
+# Queue the stats job first. This creates the graph before any other
+# jobs are run.
+queues['default'].enqueue(jobs.calculate_stats)
+queues['default'].enqueue(jobs.update_graph)
+
+# Start the scheduler
+sched.start()
---
+++ deploy/docker/worker/work.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import logging
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] [%(levelname)s] %(message)s')
+
+from redis import StrictRedis
+from rq import Worker, Queue, Connection
+
+import sys
+# The RDF package installs to the root of dist-packages...
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+# Manually add d1lod since it was copied & not installed with pip
+sys.path.append('/usr/lib/python2.7/dist-packages/d1lod')
+from d1lod import jobs
+
+conn = StrictRedis(host='redis', port='6379')
+queues = {
+    'default': Queue('default', connection=conn),
+    'dataset': Queue('dataset', connection=conn),
+    'export': Queue('export', connection=conn)
+}
+
+if __name__ == '__main__':
+    time.sleep(10)
+
+    with Connection(conn):
+        qs = [queues[q] for q in queues]
+        w = Worker(qs)
+        w.work()
