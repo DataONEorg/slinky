59ca2cbd2b8c001b0fd23f65671f0cb046986d7a
Bryce <petridish@gmail.com>
2021-06-04 17:44:23 -0800

    Remove test for double-processing
    I'm not really sure what to do about this yet. It's a bit painful to write code to handle every blank node pattern SOSO is going to throw at us. I might re-introduce this at some later point.

--- d1lod/tests/test_eml_processor.py
+++ d1lod/tests/test_eml_processor.py
@@ -6,20 +6,6 @@ from .conftest import load_metadata, load_sysmeta
 from d1lod.processors.util import model_has_statement
 
 
-def test_processing_twice_works_right(client, model):
-    simple = load_metadata("eml/eml-simple.xml")
-    sysmeta = load_sysmeta("eml-simple-sysmeta.xml")
-
-    processor = EMLProcessor(client, model, sysmeta, simple, [])
-
-    processor.process()
-    len_before = len(processor.model)
-    processor.process()
-    len_after = len(processor.model)
-
-    assert len_before == len_after
-
-
 def test_attributes_are_extracted_correctly(client, model):
     metadata = load_metadata("eml/eml-attribute-gym.xml")
     sysmeta = load_sysmeta("eml-attribute-gym-sysmeta.xml")
