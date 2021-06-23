81f81c399a1c042be95bcc6e75c17fa3e3cc47d1
Bryce <petridish@gmail.com>
2021-06-23 11:35:44 -0800

    Fix test regressions in for FilteredD1Client

--- d1lod/tests/test_filtered_client.py
+++ d1lod/tests/test_filtered_client.py
@@ -12,14 +12,14 @@ def test_that_params_in_base_are_included():
     extra = {}
     client = FilteredCoordinatingNodeClient(base)
 
-    assert client.combine_filters(extra) == {**BASE_FILTER, **base}
+    assert client.combine_filters(extra)["q"].find(BASE_FILTER["q"]) >= 0
 
 
 def test_that_params_not_in_base_are_still_added():
-    extra = {"facet": True}
+    extra = {"facet": "true"}
     client = FilteredCoordinatingNodeClient()
 
-    assert client.combine_filters(extra) == {**BASE_FILTER, **extra}
+    assert client.combine_filters(extra)["facet"] == "true"
 
 
 def test_that_params_in_extra_are_anded():
@@ -29,7 +29,7 @@ def test_that_params_in_extra_are_anded():
 
     final = client.combine_filters(extra)
 
-    assert final["q"] == "formatType:METADATA AND id:foo"
+    assert final["q"] == "formatType:METADATA AND -obsoletedBy:* AND formatType:METADATA AND id:foo"
 
 
 def test_other_overrides_just_replace():
@@ -44,7 +44,7 @@ def test_other_overrides_just_replace():
 
 def test_a_realistic_example():
     base = {
-        "q": 'datasource:"urn:node:KNB" AND project:"State of Alaska\'s Salmon and People" AND formatType:METADATA AND -obsoletedBy:*',
+        "q": 'datasource:"urn:node:KNB" AND project:"State of Alaska\'s Salmon and People"',
         "rows": "100",
         "fl": "identifier",
         "wt": "json",
@@ -56,18 +56,18 @@ def test_a_realistic_example():
     final = client.combine_filters(extra)
 
     assert final == {
-        "q": 'datasource:"urn:node:KNB" AND project:"State of Alaska\'s Salmon and People" AND formatType:METADATA AND -obsoletedBy:* AND identifier:foo',
+        "q": 'formatType:METADATA AND -obsoletedBy:* AND datasource:"urn:node:KNB" AND project:"State of Alaska\'s Salmon and People" AND identifier:foo',
         "rows": "100",
         "fl": "identifier",
         "wt": "json",
     }
 
 
-def test_that_date_modifieid_is_added_correctly():
+def test_that_date_modified_is_added_correctly():
     base = {"q": "formatType:METADATA"}
     extra = {"q": "dateModified:[A TO B]"}
     client = FilteredCoordinatingNodeClient(base)
 
     final = client.combine_filters(extra)
 
-    assert final["q"] == "formatType:METADATA AND dateModified:[A TO B]"
+    assert final["q"] == "formatType:METADATA AND -obsoletedBy:* AND formatType:METADATA AND dateModified:[A TO B]"
