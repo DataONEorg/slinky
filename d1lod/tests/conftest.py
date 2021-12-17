def0f9cdcb7fb47d5ecf834de06d2b3106df52a9
Thomas Thelen <tommythelen@gmail.com>
2021-12-17 10:13:00 -0800

    Remove unused reference to the Graph class in tests

--- d1lod/tests/conftest.py
+++ d1lod/tests/conftest.py
@@ -41,31 +41,6 @@ def blazegraph_store():
 def virtuoso_store():
     return VirtuosoStore(endpoint=f'{GRAPH_HOST}:{GRAPH_PORT}')
 
-
-@pytest.fixture(scope="module")
-def graph(store):
-    namespaces = {
-        "owl": "http://www.w3.org/2002/07/owl#",
-        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-        "foaf": "http://xmlns.com/foaf/0.1/",
-        "dcterms": "http://purl.org/dc/terms/",
-        "datacite": "http://purl.org/spar/datacite/",
-        "geolink": "http://schema.geolink.org/1.0/base/main#",
-        "d1dataset": "http://dataone.org/dataset/",
-        "d1person": "http://dataone.org/person/",
-        "d1org": "http://dataone.org/organization/",
-        "d1node": "https://cn.dataone.org/cn/v1/node/",
-        "d1landing": "https://search.dataone.org/#view/",
-        "prov": "http://www.w3.org/ns/prov#",
-    }
-
-    graph = Graph("virtuoso", 8890, "test", ns=namespaces)
-
-    return graph
-
-
 @pytest.fixture
 def model():
     storage = RDF.MemoryStorage()
